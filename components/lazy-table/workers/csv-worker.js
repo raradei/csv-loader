633d4e4844348ca9c48d67fda4fb8709d3773dda
Robert Aradei <aradeirobert@gmail.com>
2024-02-15 17:30:21 +0200

    Added filtering for multiple fields and jsdoc inside the worker

--- components/lazy-table/workers/csv-worker.js
+++ components/lazy-table/workers/csv-worker.js
@@ -1,37 +1,52 @@
 onmessage = ({ data }) => {
     if (data.type === 'loadFile') loadFile(data.file, data.pageSize);
     if (data.type === 'loadPageValues') loadPage(data.pageIndex);
-    if (data.type === 'search') search(data.key, data.value);
+    if (data.type === 'search') search(data.filters);
 }
 
+/**
+ * @param {File} file
+ * @param {Number} pageSize
+ */
 const loadFile = (file, pageSize) => {
     const reader = new FileReader();
     self.pageSize = pageSize
 
-    reader.onprogress = ({ loaded, total }) => postMessage({
-        type: 'progress',
-        value: Math.round(loaded / total * 100)
-    });
-
-    reader.onload = ({ target: { result } }) => {
-        const [rawHeaders, ...rawValues] = result.trimEnd().split('\r\n');
-        const headers = rawHeaders.split(',').map(toPascalCase);
-
-        self.lastPageIndex = Math.round(rawValues.length / pageSize - 1);
-        self.fileValues = rawValues.slice(0, pageSize).map(csvRowToObject(headers));
-
-        postMessage({ type: 'fileLoaded', value: { headers, values: self.fileValues } });
-
-        if (self.lastPageIndex) {
-            self.fileValues = self.fileValues.concat(rawValues.slice(pageSize).map(csvRowToObject(headers)));
-        }
-
-        self.currentValuesRef = self.fileValues;
-    };
-
+    reader.onprogress = onprogressHandler;
+    reader.onload = onloadHandler;
     reader.readAsText(file);
 }
 
+/**  Emits the progress when loading the file
+ * @param {ProgressEvent<FileReader>} event
+ */
+const onprogressHandler = ({ loaded, total }) => postMessage({
+    type: 'progress',
+    value: Math.round(loaded / total * 100)
+});
+
+/**  Emits the progress when loading the file
+ * @param {ProgressEvent<FileReader>} event
+ */
+const onloadHandler = ({ target: { result } }) => {
+    const [rawHeaders, ...rawValues] = result.trimEnd().split('\r\n');
+    const headers = rawHeaders.split(',').map(label => ({ label, key: toPascalCase(label) }));
+
+    self.lastPageIndex = Math.round(rawValues.length / pageSize - 1);
+    self.fileValues = rawValues.slice(0, pageSize).map(csvRowToObject(headers));
+
+    postMessage({ type: 'fileLoaded', value: { headers, values: self.fileValues } });
+
+    if (self.lastPageIndex) {
+        self.fileValues = self.fileValues.concat(rawValues.slice(pageSize).map(csvRowToObject(headers)));
+    }
+
+    self.currentValuesRef = self.fileValues;
+};
+
+/**  Loads the page at the specified index
+ * @param {number} pageIndex
+ */
 const loadPage = (pageIndex) => {
     if (pageIndex <= self.lastPageIndex) {
         const startIndex = pageIndex * self.pageSize;
@@ -47,8 +62,18 @@ const loadPage = (pageIndex) => {
     }
 }
 
-const search = (key, value) => {
-    self.currentValuesRef = self.fileValues.filter(obj => obj[key].includes(value));
+/**  Filters loaded data and emits the first page of the results
+ * @param {Map<string, object>} filters
+ */
+const search = (filters) => {
+    self.currentValuesRef = self.fileValues.filter(obj => {
+        for (let [key, value] of filters) {
+            if (!obj[key].includes(value)) return false;
+        }
+
+        return true;
+    });
+
     self.lastPageIndex = Math.round(self.currentValuesRef.length / self.pageSize - 1);
 
     postMessage({
@@ -57,9 +82,15 @@ const search = (key, value) => {
     });
 }
 
+/**
+ * Takes in a string with spaces and returns it in pascalCase
+ * @param {string} str
+ * @returns {string}
+ */
 const toPascalCase = (str) => {
     let pascalCaseProp = '';
     let isWordStart = false;
+
     for (const char of str) {
         if (char === ' ') {
             isWordStart = true;
@@ -72,12 +103,14 @@ const toPascalCase = (str) => {
         } else pascalCaseProp += char.toLowerCase();
     }
 
-    return {
-        key: pascalCaseProp,
-        label: str
-    };
+    return pascalCaseProp;
 }
 
+/**
+ * Takes in a string with spaces and returns it in pascalCase
+ * @param {{key: string, label: string}[]} headers
+ * @returns {function(csvRow: string): object}
+ */
 const csvRowToObject = (headers) => (csvRow) => {
     return csvRow.split(',').reduce((acc, val, rIdx) => {
         // TODO: Handle string that contains "
