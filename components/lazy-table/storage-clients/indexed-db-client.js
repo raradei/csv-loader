71d0dbbc7f0e07125d064892e53670f8d9bc2ad2
Robert Aradei <aradeirobert@gmail.com>
2024-02-20 21:20:35 +0200

    Export done for indexeddb

--- components/lazy-table/storage-clients/indexed-db-client.js
+++ components/lazy-table/storage-clients/indexed-db-client.js
@@ -18,24 +18,18 @@ export default async function createIndexedDBClient() {
     }
 
     function getEntries(page, size, filters = undefined) {
-        console.log(filters);
         let startIndex = (page - 1) * size;
-        // const endIndex = startIndex + size;
 
         return new Promise((resolve, reject) => {
             const request = database.transaction(storeName, 'readonly')
                 .objectStore(storeName)
                 .openCursor();
-            // .getAll(IDBKeyRange.bound(startIndex, endIndex, true, false));
 
             const result = [];
             let idx = 0;
-            let skipped = false;
+
+            request.onerror = console.log;
             request.onsuccess = ({ target: { result: cursor } }) => {
-                // if (!skipped && startIndex) {
-                //     skipped = true;
-                //     cursor.advance(startIndex);
-                // } else
                 if (cursor && filters && !isObjMatched(cursor.value, filters)) cursor.continue();
                 else {
                     if (cursor && !startIndex) {
@@ -47,6 +41,7 @@ export default async function createIndexedDBClient() {
                     else resolve({
                         entries: result,
                         currentPage: page,
+                        done: !cursor
                     });
                 }
             };
